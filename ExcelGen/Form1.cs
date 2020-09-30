127abd3356067bfd25a54b4ad7893cfc1314ceeb
barcasl3 <barcaasl3@gmail.com>
2020-09-30 11:00:42 +0200

    tasks 2-5

--- ExcelGen/Form1.cs
+++ ExcelGen/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelGen
 {
     public partial class Form1 : Form
     {
+
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            flats = context.Flats.ToList();
+        }
+
+        void CreateExcel()
+        {
+
         }
     }
 }
