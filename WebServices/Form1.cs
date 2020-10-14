bf5daab6ce562a5eb1869924ba42c3f95a44dd69
barcasl3 <barcaasl3@gmail.com>
2020-10-14 13:06:22 +0200

    task 06

--- WebServices/Form1.cs
+++ WebServices/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using WebServices.Entities;
 using WebServices.MnbServiceReference;
@@ -23,7 +24,9 @@ namespace WebServices
         {
             InitializeComponent();
             init();
+            initChart();
             dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
         }
 
         private void init()
@@ -58,6 +61,24 @@ namespace WebServices
                     rate.Value = value / unit;
                 }
             }
+        }
+
+        private void initChart()
+        {
+            var series = chartRateData.Series[0];
+
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
         }
     }
 }
