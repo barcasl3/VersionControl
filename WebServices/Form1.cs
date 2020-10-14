8028f2fe10876f5a5f7a5a642d430f863aec904a
barcasl3 <barcaasl3@gmail.com>
2020-10-14 12:25:09 +0200

    tasks 03-04

--- WebServices/Form1.cs
+++ WebServices/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebServices.Entities;
+using WebServices.MnbServiceReference;
 
 namespace WebServices
 {
     public partial class Form1 : Form
     {
+
+        MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
         public Form1()
         {
             InitializeComponent();
+            init();
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void init()
+        {
+            var request = new GetExchangeRatesRequestBody
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
